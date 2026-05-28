6f8250e5f95a8f577d0eb4b674a04ce5e3e5c93e
phuongtran31231027815 <phuongtran.31231027815@st.ueh.edu.vn>
2026-05-28 22:54:41 +0700

    Update xulydangnhap.php
    sửa chi tiết đăng nhập: nếu tài khoản bị khóa thì không đăng nhập được

--- controllers/xulydangnhap.php
+++ controllers/xulydangnhap.php
@@ -8,19 +8,34 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
     $username = trim($_POST["username"]);
     $password = trim($_POST["password"]);    
     taoKetNoi($link); 
+
+    // --- CHỐT CHẶN: KIỂM TRA TRẠNG THÁI TÀI KHOẢN ---
+    $sql_check = "SELECT trangThai FROM nguoidung WHERE tenDangNhap = ?";
+    $stmt_check = mysqli_prepare($link, $sql_check);
+    mysqli_stmt_bind_param($stmt_check, "s", $username);
+    mysqli_stmt_execute($stmt_check);
+    $res_check = mysqli_stmt_get_result($stmt_check);
+    
+    if ($row_check = mysqli_fetch_assoc($res_check)) {
+        if ($row_check['trangThai'] === 'bi_khoa') {
+            giaiPhongBoNho($link, true);
+            // Nếu bị khóa, đá thẳng về trang đăng nhập kèm thông báo "locked"
+            header("Location: ../views/dangnhap.php?msg=locked");
+            exit();
+        }
+    }
+    // --------------------------------------------------
+
     $us_module = new users_module();
-    // Model trả về nguyên một Object NguoiDung (hoặc null nếu sai tài khoản)
     $user = $us_module->dangnhap($link, $username, $password); 
 
     if ($user !== null) {
-        // Dùng phương thức Getter của Object $user để lưu vào Session hệ thống
         $_SESSION['tenDangNhap'] = $user->getTenDangNhap();
         $_SESSION['maNguoiDung'] = $user->getMaNguoiDung();
         $_SESSION['maQuyen']     = $user->getMaQuyen();
         giaiPhongBoNho($link, true);
 
-        // KIỂM TRA QUYỀN ĐỂ ĐIỀU HƯỚNG
-        
+        // Phân quyền điều hướng
         if ($_SESSION['maQuyen'] == 1) { 
             header("Location: ../controllers/admin_user_controller.php");
         } else {
