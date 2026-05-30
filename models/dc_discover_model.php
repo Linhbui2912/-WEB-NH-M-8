15a9173133b3158076d5c563c213b78f1df57f89
phuongtran31231027815 <phuongtran.31231027815@st.ueh.edu.vn>
2026-05-30 16:11:20 +0700

    Update dc_discover_model.php
    fix loi api binh luan

--- models/dc_discover_model.php
+++ models/dc_discover_model.php
@@ -17,10 +17,32 @@ class DiscoverModel {
         if ($result && mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 $row['soLuotPaw'] = (int)$row['soLuotPaw'];
+                
+                $maBaiDang = mysqli_real_escape_string($link, $row['maBaiDang']);
+                
+                
+                $queryComments = "SELECT n.tenDangNhap AS username, bl.noiDungBinhLuan AS text 
+                                  FROM binhluan bl 
+                                  JOIN nguoidung n ON bl.maNguoiDung = n.maNguoiDung 
+                                  WHERE bl.maBaiDang = '$maBaiDang'
+                                  ORDER BY bl.thoiGian ASC"; 
+                                  
+                $resComments = chayTruyVanTraVeDL($link, $queryComments);
+                $comments = [];
+                
+                if ($resComments && mysqli_num_rows($resComments) > 0) {
+                    while ($cmt = mysqli_fetch_assoc($resComments)) {
+                        $comments[] = $cmt;
+                    }
+                }
+                
+                $row['comments'] = $comments; 
+                // -------------------------------------------
+                
                 $posts[] = $row;
             }
         }
         return $posts;
     }
 }
-?>
+?> 
